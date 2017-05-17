414ab7be5aaec8af36ddab4550f13ea31777a7d0
sknapp88 <knapp533@gmail.com>
2017-05-17 14:57:17 -0400

    added bamazonCustomer.js

---
+++ bamazonCustomer.js
@@ -0,0 +1,14 @@
+var mysql = require("mysql");
+var inquirer = require("inquirer");
+
+var connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+
+  // Your username
+  user: "root",
+
+  // Your password
+  password: 'youngthing90',
+  database: "bamazondb"
+});
