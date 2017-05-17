f6f78d66dacce7a295e8a754e55520a5b8297ef4
sknapp88 <knapp533@gmail.com>
2017-05-17 15:03:12 -0400

    modified bamazonCustomer.js

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -12,3 +12,32 @@ var connection = mysql.createConnection({
   password: 'youngthing90',
   database: "bamazondb"
 });
+
+connection.connect(function(err) {
+  if (err) throw err;
+});
+
+var start = function() {
+
+  inquirer.prompt({
+    name: "itemSelect",
+    type: "rawlist",
+    message: "what item woud you like to purchase?",
+    choices: function() {
+          var choiceArray = [];
+          for (var i = 0; i < results.length; i++) {
+            choiceArray.push(results[i].item_name);
+          }
+          return choiceArray;
+        },
+  }).then(function(answer) {
+    if (answer.postOrBid.toUpperCase() === "POST") {
+      postAuction();
+    }
+    else {
+      bidAuction();
+    }
+  });
+};
+
+start();
